3d05f8afb2666f2af9e6fc8e3a2582f81217c16f
June Kim <godlyjune@gmail.com>
2021-05-19 00:20:21 +0900

    Add redux store in renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from './controllers';
 import Router from './pages/Router';
 import './App.global.scss';
 
+const store = configureStore();
+
 const App = () => {
-  return <Router />;
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  );
 };
 
 export default App;
